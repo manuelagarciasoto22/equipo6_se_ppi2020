f8f5a453ba26c0c58f06c84ad56b6fb8c75d959b
Derly Viviana Murcia Serrano <49003154+derlyvivianamurcia@users.noreply.github.com>
2020-11-25 14:56:48 -0500

    Dependencias

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import $ from "jquery";
-import popper from "../src/index.js";
-import APP from "../src/Routes/APP";
-import Pagina_principal from "../src/Pages/Pagina_principal";
+
 import App from "../src/Routes/APP";
-import Notificaciones_docentes from "../src/Pages/Notificaciones_docentes";
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
